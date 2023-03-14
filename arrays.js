fc5e58753d140da4f0f799da96cc264bcd56746b
ruthhawino <awinoruthh@gmail.com>
2023-03-14 10:46:01 +0300

    first commit

---
+++ arrays.js
@@ -0,0 +1,46 @@
+//Find the last element of the following arrays.
+arr1 = [3,7,34,90,12]
+console.log(arr1[arr1.length -1 ]);
+
+arr2 = [true,"green","where",12,56]
+console.log(arr2[arr2.length -1 ]);
+
+//Write a js programe that will join the folowing array element into a string
+myPets = ["Cow","Bird","Snake","Dog"];
+let pets = myPets.join("and");
+console.log(myPets);
+
+//Write a JS script to sort the following array items
+var arr3 = [-5,9,5,3,2,-3,6,8,4,1];
+arr3.sort();
+console.log(arr3);
+
+//Write a programe to  remove dublicates from the folowing array
+//console array with dublicate and onother without dublicate
+var arr = ["apple","mango","apple",
+"orange","mango","mango"];
+console.log(arr);
+
+
+function removeDublicates(arr){
+     return arr.filter((item,index)=> arr.indexOf(item)===index);
+ }
+ console.log (removeDublicates(arr));
+
+//Write a js script to search for the following word in the array.
+//if the word is present console it else console"The search word was not found"
+ let arr5 = ["the","way","x",4];
+int ()
+ { arr5 ="the","way","x",4;
+ if(isValidarra5(we))
+cout << "console";
+ else
+ cout << "The search word was not found";
+ return null;
+
+}
+
+// Write a JS script to sort the following string
+let word = "sevink"
+word.sort(word);
+console.log(word);
